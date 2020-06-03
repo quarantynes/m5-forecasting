b897abf74fa131fb474913c25540581cdcb9fb71
rafonseca <renanfonseca@gmail.com>
2020-06-04 00:38:05 +0200

    add parameters for training, validation and submission ranges

--- m5/params.py
+++ m5/params.py
@@ -2,12 +2,14 @@ import os.path
 
 window = 28
 nb_items = 30490
-nb_items = 4000
 nb_agg_items = 42840
 gru_size = 10
 batch_size = 16
 days = 1913
-model_name = "v1e"
 logdir = os.path.expanduser("~/tf_log")
 nb_epochs = 2
 evaluation_period = 100
+
+training_days = days - 28
+validation_range = (training_days, training_days+28)
+submission_range = (training_days+28, training_days+56)
