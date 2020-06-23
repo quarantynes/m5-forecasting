d76cc94f1364bb72d33fb2b5711dabf8bbe9be68
rafonseca <renanfonseca@gmail.com>
2020-06-23 18:42:10 +0200

    add debug module

---
+++ m5/debug.py
@@ -0,0 +1,21 @@
+# import pdb
+
+# import trepan
+import matplotlib
+
+matplotlib.use("qt5agg")
+
+import matplotlib.pyplot as plt
+
+plt.ion()
+import seaborn as sns
+
+import pandas as pd
+
+
+def hist_layer(layer):
+    data = layer.weights[0].numpy().squeeze()
+    return pd.Series(data).hist(bins=100)
+
+
+import trepan
--- m5/train.py
+++ m5/train.py
@@ -1,3 +1,4 @@
+import m5.debug
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import layers
